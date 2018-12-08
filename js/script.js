144fa739ac0f31dfd5a9dd4fc2994b975f254a2e
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-08 11:30:38 +0100

    Fix initialize and add autoplay to carousel

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 const elem = document.querySelector('.main-carousel');
-let flkty = new Flickity( elem, {
+const flkty = new Flickity( elem, {
+  autoPlay: true,
   cellAlign: 'left',
   contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-let flkty = new Flickity( '.main-carousel', {
-  // options
 });
